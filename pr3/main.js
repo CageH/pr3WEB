a03faa4023ee9aefd69352c17bd1fb1862246a25
CageH <86879842+CageH@users.noreply.github.com>
2022-12-13 11:01:20 +0300

    Add files via upload

---
+++ pr3/main.js
@@ -0,0 +1,8 @@
+const firstRow = 'мама мыла раму';
+const secondRow = 'собака друг человека';
+console.log('secondRow');
+function getRow(firstRow, secondRow) {
+
+}
+
+//console.log(getRow(firstRow, secondRow)); 
